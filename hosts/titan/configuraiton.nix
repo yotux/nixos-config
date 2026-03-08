e810bfcdf8270736c4a01a1629938b5d1ad6c2b4
yotux <github@msgnate.com>
2026-03-08 16:29:00 -0500

    bad config

--- hosts/titan/configuraiton.nix
+++ hosts/titan/configuraiton.nix
@@ -20,15 +20,11 @@
   networking.hostName = "titan"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
+  
   # Set your time zone.
   time.timeZone = "America/Chicago";
 
@@ -72,16 +68,7 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.nmurray = {
@@ -92,10 +79,9 @@
       kdePackages.kate
       tree
       writedisk
-    #  thunderbird
     ];
   };
-
+  
   programs.git = {
   enable = true;
   config = {
@@ -105,12 +91,6 @@
   };
  };
 
-environment.sessionVariables = {
-  EDITOR = "nvim";
-  # any other vars...
-  };
-  programs.fish.enable = true;
-  users.users.nmurray.shell = pkgs.fish;
   # Install firefox.
   programs.firefox.enable = true;
 
@@ -120,11 +100,6 @@ environment.sessionVariables = {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-#  (pkgs.writeShellScriptBin "nix-clean" (builtins.readFile .../scripts/nix-clean.sh))
-#  (pkgs.writeShellScriptBin "nix-usage" (builtins.readFile .../scripts/nix-usage.sh))
-#  (pkgs.writeShellScriptBin "nix-nuclear" (builtins.readFile .../scripts/nix-nuclear.sh))
   neovim
   ghostty
   git
