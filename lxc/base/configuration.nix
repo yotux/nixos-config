642eb57493275914b6bc941f12fe03e5ed08bc5a
yotux <github@msgnate.com>
2026-07-04 20:33:29 -0500

    add dhcp networking to lxc base template

--- lxc/base/configuration.nix
+++ lxc/base/configuration.nix
@@ -2,9 +2,25 @@
 
   boot.isContainer = true;
 
-  # Override proxmox-lxc default
-  networking.useDHCP = lib.mkForce true;
+  # Networking - DHCP by default
+  # For static IP, comment out DHCP section and uncomment static section
+  networking.useDHCP = lib.mkForce false;
 
+  systemd.network.enable = true;
+  systemd.network.networks."10-eth0" = {
+    matchConfig.Name = "eth0";
+    networkConfig = {
+      ## DHCP (default) - comment out for static IP
+      DHCP = "yes";
+
+      ## Static IP - uncomment and set values per container
+      ## Address = "10.10.40.101/24";
+      ## Gateway = "10.10.40.1";
+      ## DNS = "10.10.40.1";
+    };
+  };
+
+  # SSH - key only, no passwords ever
   services.openssh = {
     enable = true;
     settings = {
@@ -14,12 +30,14 @@
     };
   };
 
+  # Your FIDO2 SSH public key
   users.users.root = {
     openssh.authorizedKeys.keys = [
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIKq0ken4RRMwP6Vp/H6tQ3QaiDIId/JGatNg9rdjnweFAAAABHNzaDo= nitrokey-fido2"
     ];
   };
 
+  # Generate age key on first boot for sops-nix
   systemd.services.generate-sops-key = {
     description = "Generate age key for sops-nix";
     wantedBy = [ "multi-user.target" ];
@@ -35,6 +53,7 @@
     '';
   };
 
+  # Base packages every LXC gets
   environment.systemPackages = with pkgs; [
     borgbackup
     borgmatic
