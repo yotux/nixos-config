41bd186f77d24db5c05552d439117255f4883477
yotux <github@msgnate.com>
2026-07-17 16:25:24 -0500

    Move Caddy to LXC 100 (10.10.40.10) to match ID/10 convention

--- lxc/caddy/configuration.nix
+++ lxc/caddy/configuration.nix
@@ -5,7 +5,7 @@
   systemd.network.networks."50-eth0" = {
     matchConfig.Name = "eth0";
     networkConfig = {
-      Address = "10.10.40.101/24";
+      Address = "10.10.40.10/24";
       Gateway = "10.10.40.1";
       DNS = "10.10.40.1";
     };
@@ -14,7 +14,7 @@
 
   myModules.caddy = {
     enable = true;
-    virtualHosts."10.10.40.101:80".extraConfig = ''
+    virtualHosts."10.10.40.10:80".extraConfig = ''
       respond "Caddy is working on hive"
     '';
   };
