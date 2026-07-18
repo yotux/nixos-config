367b6c501086892611a8e21693b17b20906b5716
yotux <github@msgnate.com>
2026-07-18 14:39:00 -0500

    Add skip_install_trust to fix local PKI root cert install failure

--- lxc/caddy/configuration.nix
+++ lxc/caddy/configuration.nix
@@ -9,20 +9,35 @@
     };
     linkConfig.RequiredForOnline = "routable";
   };
+  sops.secrets.cloudflare-token = {
+    sopsFile = ../../secrets/caddy/cloudflare.yaml;
+    owner = "caddy";
+  };
+  systemd.services.caddy.serviceConfig.EnvironmentFile = config.sops.secrets.cloudflare-token.path;
   myModules.dnclient.enable = true;
   myModules.caddy = {
     enable = true;
+    package = pkgs.caddy.withPlugins {
+      plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
+      hash = "sha256-EXZTsf9KrIAi9gHsBHrYQ7oIQiYmLj6sYuQP5QihPcA=";
+    };
     virtualHosts = {
       "10.10.40.10:80".extraConfig = ''
         respond "Caddy is working on hive"
       '';
       "mygarage.naterslab.com".extraConfig = ''
         reverse_proxy 10.10.40.60:8686
-        tls internal
+        tls {
+          dns cloudflare {env.CF_API_TOKEN}
+          resolvers 1.1.1.1
+        }
       '';
       "cloud.naterslab.com".extraConfig = ''
         reverse_proxy 10.10.40.70:80
-        tls internal
+        tls {
+          dns cloudflare {env.CF_API_TOKEN}
+          resolvers 1.1.1.1
+        }
       '';
     };
   };
