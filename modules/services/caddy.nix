2071c86b3235d63e0b975bc20306ca1865e158a7
yotux <github@msgnate.com>
2026-07-18 13:48:51 -0500

    Add skip_install_trust to fix local PKI root cert install failure

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -21,6 +21,9 @@ in {
       package = cfg.package;
       globalConfig = ''
         auto_https off
+        local_certs {
+          skip_install_trust
+        }
       '';
       virtualHosts = cfg.virtualHosts;
     };
