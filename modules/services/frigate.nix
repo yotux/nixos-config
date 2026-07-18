03dc4ce730b568ed3a6099030501868e10e929f7
yotux <github@msgnate.com>
2026-07-18 18:52:15 -0500

    Add camera RTSP credentials to sops (7 secrets, mounted as individual files matching Frigate's FRIGATE_* convention)

--- modules/services/frigate.nix
+++ modules/services/frigate.nix
@@ -10,9 +10,46 @@ in {
     virtualisation.docker.enable = true;
     virtualisation.oci-containers.backend = "docker";
 
-    sops.secrets.frigate-plus-api-key = {
-      sopsFile = ../../secrets/frigate/env.yaml;
-      owner = "root";
+    sops.secrets = {
+      frigate-plus-api-key = {
+        sopsFile = ../../secrets/frigate/env.yaml;
+        owner = "root";
+      };
+      "frigate/FRIGATE_MQTT_PASSWORD" = {
+        sopsFile = ../../secrets/frigate/camera-passwords.yaml;
+        key = "mqtt-password";
+        owner = "root";
+      };
+      "frigate/FRIGATE_LIVING_ROOM_PASSWORD" = {
+        sopsFile = ../../secrets/frigate/camera-passwords.yaml;
+        key = "living-room-password";
+        owner = "root";
+      };
+      "frigate/FRIGATE_KITCHEN_PASSWORD" = {
+        sopsFile = ../../secrets/frigate/camera-passwords.yaml;
+        key = "kitchen-password";
+        owner = "root";
+      };
+      "frigate/FRIGATE_FRONT_DOOR_PASSWORD" = {
+        sopsFile = ../../secrets/frigate/camera-passwords.yaml;
+        key = "front-door-password";
+        owner = "root";
+      };
+      "frigate/FRIGATE_DRIVEWAY_PASSWORD" = {
+        sopsFile = ../../secrets/frigate/camera-passwords.yaml;
+        key = "driveway-password";
+        owner = "root";
+      };
+      "frigate/FRIGATE_GARAGE_INSIDE_PASSWORD" = {
+        sopsFile = ../../secrets/frigate/camera-passwords.yaml;
+        key = "garage-inside-password";
+        owner = "root";
+      };
+      "frigate/FRIGATE_GARAGE_ONVIF_PASSWORD" = {
+        sopsFile = ../../secrets/frigate/camera-passwords.yaml;
+        key = "garage-onvif-password";
+        owner = "root";
+      };
     };
 
     virtualisation.oci-containers.containers.frigate = {
@@ -21,6 +58,13 @@ in {
       volumes = [
         "/home/nmurray/frigate/config:/config"
         "/mnt/data:/media/frigate"
+        "${config.sops.secrets."frigate/FRIGATE_MQTT_PASSWORD".path}:/run/secrets/FRIGATE_MQTT_PASSWORD:ro"
+        "${config.sops.secrets."frigate/FRIGATE_LIVING_ROOM_PASSWORD".path}:/run/secrets/FRIGATE_LIVING_ROOM_PASSWORD:ro"
+        "${config.sops.secrets."frigate/FRIGATE_KITCHEN_PASSWORD".path}:/run/secrets/FRIGATE_KITCHEN_PASSWORD:ro"
+        "${config.sops.secrets."frigate/FRIGATE_FRONT_DOOR_PASSWORD".path}:/run/secrets/FRIGATE_FRONT_DOOR_PASSWORD:ro"
+        "${config.sops.secrets."frigate/FRIGATE_DRIVEWAY_PASSWORD".path}:/run/secrets/FRIGATE_DRIVEWAY_PASSWORD:ro"
+        "${config.sops.secrets."frigate/FRIGATE_GARAGE_INSIDE_PASSWORD".path}:/run/secrets/FRIGATE_GARAGE_INSIDE_PASSWORD:ro"
+        "${config.sops.secrets."frigate/FRIGATE_GARAGE_ONVIF_PASSWORD".path}:/run/secrets/FRIGATE_GARAGE_ONVIF_PASSWORD:ro"
       ];
       ports = [
         "8971:8971"
