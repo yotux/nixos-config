24368083bb483ccdb273f87063d083dcdc0fa4fb
yotux <github@msgnate.com>
2026-08-28 23:32:55 -0500

    hyprland edits

--- home/nmurray/hyprland.nix
+++ home/nmurray/hyprland.nix
@@ -45,7 +45,8 @@
         "SUPER SHIFT, 3, movetoworkspace, 3"
         "SUPER SHIFT, 4, movetoworkspace, 4"
 
-        "SUPER, PRINT, exec, grim -g \"$(slurp)\" - | wl-copy"
+#        "SUPER, PRINT, exec, grim -g \"$(slurp)\" - | wl-copy"
+	"SUPER, PRINT, exec, ~/bin/hypr-screenshot"
       ];
     };
   };
